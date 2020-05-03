8007fd99b5c9bfaaa4d5190540c5980c17405350
Juan Miguel Guerrero <juanmiguel@existo.es>
2020-05-04 01:27:31 +0200

    Puppeteer disable-setuid-sandbox config for heroku

--- scraper.js
+++ scraper.js
@@ -12,7 +12,7 @@ async function addressScraper(query) {
 	console.clear()
 
 	// const browser = await puppeteer.launch({headless: false})
-	const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
+	const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
 	const page = await browser.newPage()
 	
 	// await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1')
